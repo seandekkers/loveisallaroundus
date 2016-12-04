33ea2f33235a5e7cc5c82758201b06c72c0bf811
seandekkers <sdekkers@ideo.com>
2016-12-04 15:03:23 -0800

    clock

--- clock.js
+++ clock.js
@@ -1,13 +1,13 @@
 function runDataBase(){
 
-
+	
 
 }
 
 var CronJob = require('cron').CronJob;
 new CronJob({
   cronTime: "15 * * * * *",//15 seconds after every minute
-  onTick: runDataBase,
-  start: start,
+  onTick: manageDataBase,
+  start: true,
   timeZone: "America/Los_Angeles"
 });
